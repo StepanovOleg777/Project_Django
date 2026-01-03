a0bcd16e2a9c1c5707230c668b4e7bcb6d7fac02
Олег Степанов <stepanov.2401@mail.ru>
2026-01-03 18:46:45 +0300

    еализация аутентификации: регистрация, вход, защита продуктов, отправка email

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('blog/', include('blog.urls')),
     path('users/', include('users.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
 
 if settings.DEBUG:
--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, \
+    UserChangeForm  # ← ДОБАВИТЬ AuthenticationForm
 from .models import User
 
 
 class CustomUserCreationForm(UserCreationForm):
-    """Форма для создания пользователя"""
+    """Форма для создания пользователя в админке"""
 
     class Meta:
         model = User
@@ -11,25 +12,14 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
-    """Форма для изменения пользователя"""
+    """Форма для изменения пользователя в админке"""
 
     class Meta:
         model = User
         fields = ('email',)
 
 
-class UserRegisterForm(UserCreationForm):
-    """Форма регистрации пользователя"""
-    class Meta:
-        model = User
-        fields = ('email', 'password1', 'password2')
-
-class UserLoginForm(AuthenticationForm):
-    """Форма авторизации через email"""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].label = 'Email'
-
+# === ФОРМЫ ДЛЯ РЕГИСТРАЦИИ И АВТОРИЗАЦИИ ===
 
 class UserRegisterForm(UserCreationForm):
     """Форма регистрации пользователя"""
